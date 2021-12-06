818c1cefa8771f8be8b167e7a32328c09b8f8ec5
mohammed.abdul.hakim <mahakim9283@gmail.com>
2021-12-06 12:59:29 +0530

    Changes added to EmployeeEntity.java and Controller.java

---
+++ src/main/java/com/scott/betaexam/EmployeeNotFoundException.java
@@ -0,0 +1,13 @@
+package com.scott.betaexam;
+
+import net.bytebuddy.implementation.bind.annotation.Super;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class EmployeeNotFoundException extends RuntimeException{
+    public EmployeeNotFoundException(String message) {
+        super(message);
+    }
+
+}
